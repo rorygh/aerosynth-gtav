4788031be80ef67da0586241a440cf4e55db8f5d
Rory M <rory@mcclenagan.net>
2026-05-14 22:55:21 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Aerosynth-GTAV
+
+Synthetic data pipeline source for GTA V
+
+## Dependencies
+
+* CMake (developed on 4.3.2)
+* MVSC (developed on MSVC 19.51.36243.0)
+
+The project includes a vendored copy of ScriptHookV SDK 1.0.617.1a.
+
+## Building
+
+```
+cmake -B build
+cmake --build build --config Release
+```
---
+++ src/script.h
@@ -0,0 +1,15 @@
+/*
+	THIS FILE IS A PART OF GTA V SCRIPT HOOK SDK
+				http://dev-c.com
+			(C) Alexander Blade 2015
+*/
+
+#pragma once
+
+#include "scripthookv_sdk\inc\natives.h"
+#include "scripthookv_sdk\inc\types.h"
+#include "scripthookv_sdk\inc\enums.h"
+
+#include "scripthookv_sdk\inc\main.h"
+
+void ScriptMain();
